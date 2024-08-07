cbdd8c191b8193b795d9fae8eb359c42a5a339c4
Dragan <milosevicdragan002@gmail.com>
2024-08-07 20:53:39 +0200

    add gnojs wallet

--- Projekat/scripts/listener.ts
+++ Projekat/scripts/listener.ts
@@ -1,15 +1,149 @@
 import { ethers } from "ethers";
 import * as dotenv from "dotenv";
-import { Tx } from "@gnolang/tm2-js-client";
-const { Wallet } = require('@gnolang/gno-js-client');
-import * as BRIDGE_ABI from '../abi.json';
+import * as abi from '../abi';
 
+import { GnoWallet } from "@gnolang/gno-js-client";
+
+const BRIDGE_ABI = [
+  {
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "_token",
+              "type": "address"
+          }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "internalType": "address",
+              "name": "by",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "amount",
+              "type": "uint256"
+          },
+          {
+              "indexed": false,
+              "internalType": "address",
+              "name": "gno_address",
+              "type": "address"
+          }
+      ],
+      "name": "Deposit",
+      "type": "event"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "gno_addr",
+              "type": "address"
+          }
+      ],
+      "name": "deposit",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "name": "deposits",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "_addr",
+              "type": "address"
+          }
+      ],
+      "name": "getDeposit",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [],
+      "name": "index",
+      "outputs": [
+          {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "inputs": [
+          {
+              "internalType": "address",
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "internalType": "uint256",
+              "name": "_amount",
+              "type": "uint256"
+          }
+      ],
+      "name": "release",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "inputs": [],
+      "name": "token",
+      "outputs": [
+          {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+          }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+  }
+]
 
 async function main() {
   const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_URL);
   const bridgeContract = new ethers.Contract(process.env.BRIDGE_CONTRACT_ADDRESS, BRIDGE_ABI, provider);
   const mnemonic = 'upgrade disagree dilemma diagram tunnel connect glue finger insane heavy vintage stadium tuition pizza embark budget circle shop buddy joke walk intact soon illegal';
-  const wallet = await Wallet.fromMnemonic(mnemonic);
+  const wallet = await GnoWallet.fromMnemonic(mnemonic);
+
 
   console.log(`Listening for Deposit events on ${process.env.BRIDGE_CONTRACT_ADDRESS}`);
 
@@ -19,8 +153,9 @@ async function main() {
     console.log(`By: ${by}`);
     console.log(`Amount: ${ethers.formatUnits(amount, 18)}`);
     console.log(`GNO Address: ${gno_address}`);
-    await wallet.callMethod('gno.land/r/demo/foo20', 'TotalBalance', []);
-    
+    amount = ethers.formatUnits(amount, 18);
+    const txHash = await wallet.callMethod('gno.land/r/g15mzjefvj9pt2ctv30l9ju03rewmfv9hken9wfm/v1/bridge', 'Mint', [amount,gno_address],"https://rpc.test4.gno.land:443");
+    console.log(`Transaction hash: ${txHash}`)
   };
 
   bridgeContract.on("Deposit", handleDepositEvent);
